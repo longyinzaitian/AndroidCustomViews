35867e76c594ee89ad114ab7f1bc94f07c3a3834
longyinzaitian <1536132397@qq.com>
2019-02-20 01:29:42 +0800

    update

--- app/src/main/java/com/example/customview/rvpg/CardDetailFragment.java
+++ app/src/main/java/com/example/customview/rvpg/CardDetailFragment.java
@@ -2,12 +2,17 @@ package com.example.customview.rvpg;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.customview.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  *
@@ -16,6 +21,12 @@ import com.example.customview.R;
  * @date 2019年2月17日
  */
 public class CardDetailFragment extends Fragment {
+
+    @BindView(R.id.btn_normal)
+    Button btnNormal;
+
+    @BindView(R.id.btn_exp_1)
+    Button btnExp1;
 
     private static CardDetailFragment mInstance = null;
     public CardDetailFragment() {
@@ -43,10 +54,15 @@ public class CardDetailFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull  LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_card_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_card_detail, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
-
-
-
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.btnNormal.setOnClickListener((View v) -> System.out.println("card detail fragment."));
+        this.btnExp1.setOnClickListener((View v) -> System.out.println("car detail fragment. btx exp1"));
+    }
 }
